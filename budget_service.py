2330eca1cfdbde33b5e8b9dab2a5704f98923170
leslie_liu <leslie_liu@trendmicro.com>
2021-12-01 16:23:41 +0800

    add tests

--- budget_service.py
+++ budget_service.py
@@ -18,7 +18,11 @@ class BudgetService(IBudgetReoo):
         self.current_v = 0
 
     def query(self, start_date, end_date) -> float:
+
         days = -1
+
+        lookup = {}
+
         for budget in self.budgets:
             print(f'key: {budget.year_month}, value: {budget.amount}')
             year = str(start_date)[:4]
@@ -26,14 +30,15 @@ class BudgetService(IBudgetReoo):
             days = monthrange(int(year), int(month))[1]
             print('Number of days: {}'.format(days))
             if year == budget.year_month[:4] and month == budget.year_month[4:6]:
-                self.current_v = budget.amount
+                lookup[budget.year_month] = budget.amount
+                # self.current_v = budget.amount
 
         # get month budget
-        if days < 0:
+        diff = (end_date - start_date).days
+        if days < 0 or diff < 0:
             return 0
-        diff = (end_date - start_date).days + 1
         print(f"diff: {diff}")
-        result = diff * self.current_v // days
+        result = (diff + 1) * self.current_v // days
         return result
 
     def get_all(self, budgets) -> list:
--- budget_service_test.py
+++ budget_service_test.py
@@ -20,7 +20,7 @@ class BudgetServiceTests(unittest.TestCase):
         start_date = date(2020, 6, 1)
         end_date = date(2020, 6, 1)
         result = self.b.query(start_date, end_date)
-        assert result == 1
+        self.assertEqual(result, 1)
 
     def test_query_one_day_without_db_data(self):
         mock_budget = []
@@ -28,7 +28,7 @@ class BudgetServiceTests(unittest.TestCase):
         start_date = date(2020, 6, 1)
         end_date = date(2020, 6, 1)
         result = self.b.query(start_date, end_date)
-        assert result == 0
+        self.assertEqual(result, 0)
 
     def test_query_one_day_when_year_month_budget_is_zero(self):
         mock_budget = [Budget('202006', 0)]
@@ -36,6 +36,28 @@ class BudgetServiceTests(unittest.TestCase):
         start_date = date(2020, 6, 1)
         end_date = date(2020, 6, 1)
         result = self.b.query(start_date, end_date)
-        assert result == 0
+        self.assertEqual(result, 0)
 
+    def test_query_cross_days_same_month(self):
+        mock_budget = [Budget('202006', 30)]
+        budget = self.b.get_all(mock_budget)
+        start_date = date(2020, 6, 1)
+        end_date = date(2020, 6, 30)
+        result = self.b.query(start_date, end_date)
+        self.assertEqual(result, 30)
 
+    def test_query_invalid_cross_days(self):
+        mock_budget = [Budget('202006', 30)]
+        budget = self.b.get_all(mock_budget)
+        start_date = date(2020, 6, 30)
+        end_date = date(2020, 6, 1)
+        result = self.b.query(start_date, end_date)
+        self.assertEqual(result, 0)
+
+    def test_query_cross_month(self):
+        mock_budget = [Budget('202005', 31), Budget('202006', 60)]
+        budget = self.b.get_all(mock_budget)
+        start_date = date(2020, 5, 31)
+        end_date = date(2020, 6, 2)
+        result = self.b.query(start_date, end_date)
+        self.assertEqual(result, 5)

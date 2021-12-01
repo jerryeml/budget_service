561b66b8b55a6fd692f42246d47aca6f98d64b72
leslie_liu <leslie_liu@trendmicro.com>
2021-12-01 16:07:44 +0800

    add tests

--- budget_service.py
+++ budget_service.py
@@ -6,6 +6,7 @@
 from datetime import date
 from calendar import monthrange
 
+
 class IBudgetReoo:
     def get_all() -> list:
         pass
@@ -17,22 +18,23 @@ class BudgetService(IBudgetReoo):
         self.current_v = 0
 
     def query(self, start_date, end_date) -> float:
-        print(f'budgets: {self.budgets}')
-        for k, v in self.budgets:
-            print(f'key: {k}, value: {v}')
-            year = str(start_date)[:4] 
+        days = -1
+        for budget in self.budgets:
+            print(f'key: {budget.year_month}, value: {budget.amount}')
+            year = str(start_date)[:4]
             month = str(start_date)[5:7]
             days = monthrange(int(year), int(month))[1]
-            print( 'Number of days: {}'.format( days) )
-            if year == k[:4] and month == k[4:6]:
-                self.current_v = v 
+            print('Number of days: {}'.format(days))
+            if year == budget.year_month[:4] and month == budget.amount[4:6]:
+                self.current_v = (budget.amount or 0)
 
         # get month budget
+        if days < 0:
+            return 0
         diff = (end_date - start_date).days + 1
         print(f"diff: {diff}")
-        result =  diff * self.current_v / days
+        result = diff * self.current_v // days
         return result
-
 
     def get_all(self, budgets) -> list:
         self.budgets = budgets
@@ -41,4 +43,3 @@ class BudgetService(IBudgetReoo):
 
 if __name__ == "__main__":
     pass
-
--- budget_service_test.py
+++ budget_service_test.py
@@ -1,19 +1,41 @@
+import unittest
 
-import pytest
 from datetime import date
 from budget_service import BudgetService
 
-def setup():
-    b = BudgetService()
-    return b
 
-def test_query_one_day():
-    b = setup()
-    mock_budget = [("202006", 30)]
+class Budget:
+    def __init__(self, year_month, amount):
+        self.year_month = year_month
+        self.amount = amount
 
-    budget = b.get_all(mock_budget)
-    
-    start_date = date(2020,6,1)
-    end_date = date(2020,6,1)
-    result = b.query(start_date, end_date)
-    assert result == 1
+
+class BudgetServiceTests(unittest.TestCase):
+    def setUp(self):
+        self.b = BudgetService()
+
+    def test_query_one_day(self):
+        mock_budget = [Budget('202006', 30)]
+        budget = self.b.get_all(mock_budget)
+        start_date = date(2020, 6, 1)
+        end_date = date(2020, 6, 1)
+        result = self.b.query(start_date, end_date)
+        assert result == 1
+
+    def test_query_one_day_without_db_data(self):
+        mock_budget = []
+        budget = self.b.get_all(mock_budget)
+        start_date = date(2020, 6, 1)
+        end_date = date(2020, 6, 1)
+        result = self.b.query(start_date, end_date)
+        assert result == 0
+
+    def test_query_one_day_when_year_month_budget_is_zero(self):
+        mock_budget = [Budget('202006', 0)]
+        budget = self.b.get_all(mock_budget)
+        start_date = date(2020, 6, 1)
+        end_date = date(2020, 6, 1)
+        result = self.b.query(start_date, end_date)
+        assert result == 0
+
+
